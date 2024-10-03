ee01852e54b7ecaf76fa58efd29d78d4d7ba8678
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-03 11:26:11 -0400

    made orange

--- main.ts
+++ main.ts
@@ -45,8 +45,9 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    basic.pause(1000)
 
-    //orange
+    //yellow
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
@@ -66,7 +67,7 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P14, 0)
     basic.pause(1000)
 
-    //yellow
+    //orange
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 1)
