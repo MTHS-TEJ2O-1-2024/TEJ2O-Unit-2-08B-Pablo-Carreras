7f77b96e0824ed69bd936ed21d0817f9fd490627
Pablo-Carreras <pablo.carreras-canton@mths.ca>
2024-10-03 10:45:51 -0400

    make the code

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Pablo
  * Created on: Sep 2024
- * This program ...
+ * This program tunrs n and off and switches colours 
 */
 
 basic.clearScreen()
@@ -11,15 +11,22 @@ basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
-    basic.pause(100)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
-    basic.pause(100)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    basic.pause(100)
+    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
 })
